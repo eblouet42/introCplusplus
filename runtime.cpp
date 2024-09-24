f6325e84776f7bdf5d57067c4bed46ac7c935b1e
Evan Blouet <evan.blouet@gmail.com>
2024-09-24 16:19:11 +0200

    Creating a C-array

--- runtime.cpp
+++ runtime.cpp
@@ -1,3 +1,9 @@
-//
-// Created by evan on 24/09/2024.
-//
+#include <iostream>
+
+int main() {
+    int tablopairs[1000];
+    tablopairs[0] = 2;
+    for (int i=1; i<1000; ++i) {
+        tablopairs[i] = i*2;
+    }
+}
