aa14ff22b2a0c6240220ff44a4cd8b3684413010
Evan Blouet <evan.blouet@gmail.com>
2024-09-24 15:48:15 +0200

    hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main() {
+void printGreetings(int upperbound) {
+    for (int i = 2; i <= upperbound; i+=2) {
+            std::cout << i << std::endl;
+    }
     std::cout << "Hello, World!" << std::endl;
+    int truc=upperbound;
+    if (upperbound%2==0) {
+        truc=upperbound-1;
+    }
+    for (int i=truc; i >= 1; i-=2) {
+            std::cout << i << std::endl;
+    }
+}
+int main() {
+    printGreetings(10);
+    printGreetings(20);
     return 0;
 }
