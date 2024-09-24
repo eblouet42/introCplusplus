d93e214aa3a46e5b880378cf7deb33581276f80d
Evan Blouet <evan.blouet@gmail.com>
2024-09-24 16:37:37 +0200

    Sum runtime

--- runtime.cpp
+++ runtime.cpp
@@ -4,10 +4,17 @@
 int main() {
     int tablopairs[1000];
     auto start_time_point=std::chrono::high_resolution_clock::now();
-    tablopairs[0] = 2;
-    for (int i=1; i<1000; ++i) {
-        tablopairs[i] = i*2;
+    for (int i=0;i<1000;++i){
+        tablopairs[i]=i*2;
     }
-    auto end_time_point = std::chrono::high_resolution_clock::now();
-    std::cout << "Da tableau créé en " << end_time_point-start_time_point << std::endl;
+    auto end_time_point=std::chrono::high_resolution_clock::now();
+    std::cout << "Da tableau cree en " << end_time_point-start_time_point << std::endl;
+    auto sum_start_time_point= std::chrono::high_resolution_clock::now();
+    int sum=0;
+    for (int i=0;i<1000;++i){
+        sum+=tablopairs[i];
+    }
+    auto sum_end_time_point = std::chrono::high_resolution_clock::now();
+    std::cout << "Da somme faite en " << sum_end_time_point-sum_start_time_point << std::endl;
+    std::cout << "Anyway, da somme en question: " << sum << "\n";
 }
