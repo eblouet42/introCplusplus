a10b4199942c29d4d300235091efd9b01b7a379c
Evan Blouet <evan.blouet@gmail.com>
2024-09-24 15:58:48 +0200

    PrintGreetings
    same but with a twist: now u can choose the upper bound

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
-int main() {
-    for (int i = 2; i <= 10; i+=2) {
+void printGreetings(int upperbound) {
+    for (int i = 2; i <= upperbound; i+=2) {
         std::cout << i << std::endl;
     }
     std::cout << "Hello, World!" << std::endl;
-    for (int i=9; i >= 1; i-=2) {
+    int truc=upperbound;
+    if (upperbound%2==0) {
+        truc=upperbound-1;
+    }
+    for (int i=truc; i >= 1; i-=2) {
         std::cout << i << std::endl;
     }
+}
+int main() {
+    printGreetings(10);
+    printGreetings(20);
+    return 0;
 }
