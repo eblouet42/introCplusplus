07148812bca44f60f881cf74a112adeaa16386f4
Evan Blouet <evan.blouet@gmail.com>
2024-10-05 15:14:33 +0200

    palyndrome

--- cstring.cpp
+++ cstring.cpp
@@ -7,8 +7,34 @@ int mylen(char s[]) {
     return len;
 }
 
+int compare(char s[], char t[]) {
+    int len = mylen(t);
+    for (int i = 0; i < len; i++) {
+        if (s[i] < t[i]) {
+            return -1;
+        } if (s[i] > t[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+bool palyndrome(char s[]){
+    int length = mylen(s);
+    for (int i=0; i<length/2; ++i) {
+        if (s[i]!=s[length-i-1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     char s[] = "Hello World!";
     std::cout << mylen(s) << std::endl;
+    std::cout << compare("kakou kakou", "kakouzz")  << std::endl;
+    std::cout << compare("ohio", "amogus")  << std::endl;
+    std::cout << compare("Mathis", "hugo")  << std::endl;
+    std::cout << compare("Hello World!", "Hello World!") << std::endl;
+    std::cout << compare(" ", "") << std::endl;
     return 0;
 }
