13a12d6091e309b79d0d07008684e2817e44823d
Evan Blouet <evan.blouet@gmail.com>
2024-10-14 10:03:31 +0200

    ultimate final update
    removing the null check occuring in insert element

---
+++ dichotomy.cpp
@@ -0,0 +1,18 @@
+
+struct intpair{
+    int a=0, b=0;};
+
+struct dicoindex {
+    };
+
+int dichotomy(searchdic dic) {
+    bool fini=false;
+    while (!fini) {
+        if (dic.tablo.a==dic.tablo.b) {
+            fini=true;
+            if
+        }
+            for (int i=a)
+    }
+
+}
